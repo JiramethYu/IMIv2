738c31310247aca444b97ac848aabbd56604c06f
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-19 11:44:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,26 +15,26 @@
 </head>
 
   <body>
-    <h1>Hello, world!</h1>
-     <div class="row">
-      <div class="col-4">
-          <iframe width="600px" hight="600px" src="https://api.thingspeak.com/channels/1458765/feeds.json?results=2" frameborder="0"></iframe>
-      </div> 
-      <div class="col-4"></div> 
-      <div class="col-4"></div> 
-
-
-     </div>
     
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
    
   </body>
 
 <script>
-    
+
     $(()=>{
-        alert("Hello World");
+        //alert("Hello ");
+        let url = "https://api.thingspeak.com/channels/1458765/feeds.json?results=1 "
+        $.get(url)
+        .done(function(data){
+
+        })
+        
+        .fail(function(erroe){
+
+        })
     });
 
 </script>
