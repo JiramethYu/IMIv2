e6a46ded0e942ed8174d2aad1d9027c5f7ad6d3f
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-19 11:05:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-Mr Jirameth Yukachain
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+     <div class="row">
+      <div class="col-4">
+          <iframe src="https://api.thingspeak.com/channels/1458765/feeds.json?results=2" frameborder="0"></iframe>
+      </div> 
+      <div class="col-4"></div> 
+      <div class="col-4"></div> 
+
+
+     </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+   
+  </body>
+</html>
 
